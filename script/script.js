08be4d7cd5738ad2f0e1c9da239e7150b803cce9
Ankon 190 <ankon190@gmail.com>
2026-02-22 19:32:21 +0600

    half functionality done

--- script/script.js
+++ script/script.js
@@ -13,6 +13,7 @@ const allBtn = getValueFromId("all-btn");
 const interviewBtn = getValueFromId("interview-btn");
 const rejectedBtn = getValueFromId("rejected-btn");
 const interviewList = getValueFromId("interviewed-list");
+const rejectedList = getValueFromId("rejected-list");
 
 //console.log("all cards: ", allJobCards);
 
@@ -46,6 +47,7 @@ function toggleStyle(id){
     else if(id === "rejected-btn"){
         allJobCards.classList.add("hidden");
         interviewList.classList.add("hidden");
+        rejectedList.classList.remove("hidden");
     }
 }
 
@@ -57,22 +59,46 @@ mainContainer.addEventListener("click", function(event){
         const jobRole = card.querySelector(".job-role").innerText;
         const jobSalary = card.querySelector(".job-salary").innerText;
         const jobDescription = card.querySelector(".job-description").innerText;
-        const jobStatus = card.querySelector(".status-badge");
+        const jobStatus = card.querySelector(".status-badge").innerHTML;
         const jobData = {
             jobTitle,
             jobRole,
             jobSalary,
-            jobDescription
+            jobDescription,
+            jobStatus: "INTERVIEW"
         };
         //console.log("Job data: ", jobData);
         const jobExistsInterview = interviewed.find(job => job.jobTitle === jobTitle);
         if(!jobExistsInterview){
             interviewed.push(jobData);
-            jobStatus.innerHTML = "INTERVIEW";
             calculateCounts();
             console.log("Added to interviewed: ", jobData);
         }
         renderInterviewedList(); 
+    }
+    else if(event.target.classList.contains("rejected-btn")){
+        //console.log("Parent element:", event.target.parentNode.parentNode);
+        const card = event.target.parentNode.parentNode;
+        const jobTitle = card.querySelector(".job-title").innerText;
+        const jobRole = card.querySelector(".job-role").innerText;
+        const jobSalary = card.querySelector(".job-salary").innerText;
+        const jobDescription = card.querySelector(".job-description").innerText;
+        const jobStatus = card.querySelector(".status-badge").innerHTML;
+        const jobData = {
+            jobTitle,
+            jobRole,
+            jobSalary,
+            jobDescription,
+            jobStatus: "REJECTED"
+        };
+        //console.log("Job data: ", jobData);
+        const jobExistsInterview = rejected.find(job => job.jobTitle === jobTitle);
+        if(!jobExistsInterview){
+            rejected.push(jobData);
+            calculateCounts();
+            console.log("Added to rejected: ", jobData);
+        }
+        renderRejectedList(); 
     }
 });
 
@@ -100,9 +126,7 @@ function renderInterviewedList(){
                     
                     <!-- Status badge -->
                     <div class="not-applied mt-2">
-                        <span class="not-applied badge bg-[#EEF4FF] w-36 p-4 text-[16px] font-medium">NOT APPLIED</span>
-                        <button class="interview-show-btn btn btn-outline btn-success hidden">INTERVIEW</button>
-                        <button class="rejected-show-btn btn btn-outline btn-error hidden">REJECTED</button>
+                        <span class="status-badge badge bg-[#EEF4FF] w-36 p-4 text-[16px] font-medium">${job.jobStatus}</span>
                     </div>
                     
                     <!-- Description -->
@@ -118,6 +142,49 @@ function renderInterviewedList(){
                 </div>`;
         
         interviewList.appendChild(div);
+
+    }
+}
+
+function renderRejectedList(){
+    rejectedList.innerHTML = ""; // Clear previous content
+    //jobStatus.classList.add("hidden");
+    for(let job of rejected){
+        let div = document.createElement("div");
+        div.classList.add("card", "w-full", "bg-base-100", "my-4");
+        div.innerHTML = `<div class="card-body w-full">
+                    <!-- Header with title and delete button -->
+                    <div class="flex items-start justify-between">
+                        <div>
+                            <h2 class="job-title text-xl text-black-300">${job.jobTitle}</h2>
+                            <p class="job-role text-lg text-gray-500 mt-2">${job.jobRole}</p>
+                            <p class="job-salary text-[14px] text-gray-500 mt-2">${job.jobSalary}</p>
+                        </div>
+                        <button class="btn btn-ghost btn-circle text-gray-500 border-1 hover:bg-error/10 ">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                            </svg>
+                        </button>
+                    </div>
+                    
+                    <!-- Status badge -->
+                    <div class="not-applied mt-2">
+                        <span class="status-badge badge bg-[#EEF4FF] w-36 p-4 text-[16px] font-medium">${job.jobStatus}</span>
+                    </div>
+                    
+                    <!-- Description -->
+                    <p class="job-description text-black-500 text-[16px] mt-3">
+                        ${job.jobDescription}
+                    </p>
+                    
+                    <!-- Action buttons -->
+                    <div class="card-actions mt-4">
+                        <button class="interview-btn btn btn-outline btn-success">INTERVIEW</button>
+                        <button class="rejected-btn btn btn-outline btn-error">REJECTED</button>
+                    </div>
+                </div>`;
+        
+        rejectedList.appendChild(div);
 
     }
 }
